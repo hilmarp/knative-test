0a8abb2ef54f62b78a8c4d177a075a3e877be2c8
Hilmar Palsson <hilmar.palsson@conversocial.com>
2022-07-18 11:53:39 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ app.get('*', async (req, res) => {
     const response = await fetch('https://meowfacts.herokuapp.com/');
     const fact = await response.json();
 
-    res.json(fact);
+    res.json({
+        fact: fact.data[0],
+        random: 'stuff'
+    });
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server listening on port', port);
 });
